97666ae279010494dd161ef892e2e1a1e56e0692
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-18 16:54:39 +0900

    Adopt-me done

--- 01-adopt-me/SRC/App.js
+++ 01-adopt-me/SRC/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {},[
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
 
   ])
 }
@@ -10,10 +10,11 @@ const Pet = () => {
 const App = () => {
   return React.createElement("div",{},
   [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havenese"}),
+    React.createElement(Pet, {name: "Jacinto", animal: "Dog", breed: "Schnousser"}),
+    React.createElement(Pet, {name: "Karlota", animal: "Dog", breed: "Schnousser"}),
+
     ]
   );
 };
