b0b83ad20b3082638e6c251728ab1422f7a84b8f
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-21 13:59:41 +0900

    prettier and eslint added

--- 01-adopt-me/SRC/App.js
+++ 01-adopt-me/SRC/App.js
@@ -1,22 +1,30 @@
 const Pet = (props) => {
-  return React.createElement("div", {},[
+  return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-
-  ])
-}
-
-const App = () => {
-  return React.createElement("div",{},
-  [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havenese"}),
-    React.createElement(Pet, {name: "Jacinto", animal: "Dog", breed: "Schnousser"}),
-    React.createElement(Pet, {name: "Karlota", animal: "Dog", breed: "Schnousser"}),
-
-    ]
-  );
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havenese",
+    }),
+    React.createElement(Pet, {
+      name: "Jacinto",
+      animal: "Dog",
+      breed: "Schnousser",
+    }),
+    React.createElement(Pet, {
+      name: "Karlota",
+      animal: "Dog",
+      breed: "Schnousser",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
