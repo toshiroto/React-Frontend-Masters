0bac6548b8dd525eea59c718c291c2295ed7194f
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-21 15:22:04 +0900

    new pet component

--- 01-adopt-me/SRC/App.js
+++ 01-adopt-me/SRC/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Lunass",
+      name: "Luna",
       animal: "Dog",
       breed: "Havenese",
     }),
