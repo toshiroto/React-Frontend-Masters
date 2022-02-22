70fa5cac8042b52c667aacc331a81e5a347435c1
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-22 15:10:40 +0900

    JSX added to Pet

--- 01-adopt-me/SRC/App.js
+++ 01-adopt-me/SRC/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havenese",
-    }),
-    React.createElement(Pet, {
-      name: "Jacinto",
-      animal: "Dog",
-      breed: "Schnousser",
-    }),
-    React.createElement(Pet, {
-      name: "Karlota",
-      animal: "Dog",
-      breed: "Schnousser",
-    }),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havenese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Jacinto",
+//       animal: "Dog",
+//       breed: "Schnousser",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Karlota",
+//       animal: "Dog",
+//       breed: "Schnousser",
+//     }),
+//   ]);
+// };
 
+const App = () => {
+  return (
+  <div>
+    <h1>Adopt Me</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Jacinto" animal="Dog" breed="Schnousser" />
+    <Pet name="La dona" animal="Dog" breed="English Sheppard" />
+  </div>
+  );
+};
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
