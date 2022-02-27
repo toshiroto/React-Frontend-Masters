994f56f7dc691303368f3f2afbb4d24d66ce59ec
Carlos Hernandez Topete <chtopete@gmail.com>
2022-02-27 14:14:18 +0900

    Animal Dropdown menu

--- 01-adopt-me/SRC/SearchParams.js
+++ 01-adopt-me/SRC/SearchParams.js
@@ -1,21 +1,40 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "repitle"];
 
-
-const SearchParams = () =>  {
+const SearchParams = () => {
   const [location, setLocation] = useState("Tokyo, JP");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
-    <form>
-      <label htmlFor="location">
-        Location
-        <input id="location" onChange={e => setLocation(e.target.value)}
-        value={location}
-        placeholder="Location" />
-      </label>
-      <button>Submit</button>
-    </form>
-
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
