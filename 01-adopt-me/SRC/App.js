b7de72cee0659743fbf3f2ba7a5fdb339a84fb93
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-01 14:27:28 +0900

    Strict mode added to App.js

--- 01-adopt-me/SRC/App.js
+++ 01-adopt-me/SRC/App.js
@@ -1,36 +1,19 @@
-
-import ReactDOM from 'react-dom';
-import Pet from './Pet';
-import SearchParams from './SearchParams';
-
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Havenese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Jacinto",
-//       animal: "Dog",
-//       breed: "Schnousser",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Karlota",
-//       animal: "Dog",
-//       breed: "Schnousser",
-//     }),
-//   ]);
-// };
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-  <div>
-    <h1>Adopt Me</h1>
-    <SearchParams />
-  </div>
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
   );
 };
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
