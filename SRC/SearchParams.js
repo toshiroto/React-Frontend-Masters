0755e8b9c79a2208a7e7755eea59694bb31bfb8a
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-22 15:43:55 +0900

    on Submit event added on form

--- SRC/SearchParams.js
+++ SRC/SearchParams.js
@@ -28,7 +28,11 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
+      
         <label htmlFor="location">
           Location {location}
           <input
