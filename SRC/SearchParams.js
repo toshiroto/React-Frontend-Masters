72ff72f0da79b07d530f8b0fda248af96e2448f8
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-22 16:20:37 +0900

    Results component component added

--- SRC/SearchParams.js
+++ SRC/SearchParams.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet"
 import useBreedList from "./useBreedList";
+import Results from "./Results"
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -32,7 +32,7 @@ const SearchParams = () => {
         e.preventDefault();
         requestPets();
       }}>
-      
+
         <label htmlFor="location">
           Location {location}
           <input
@@ -83,14 +83,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results />
     </div>
   );
 };
