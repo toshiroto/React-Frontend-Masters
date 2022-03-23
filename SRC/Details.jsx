f591a76d1733693878346141409f0bd174eeb3c8
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-23 15:02:16 +0900

    Carousel added

--- SRC/Details.jsx
+++ SRC/Details.jsx
@@ -1,12 +1,11 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
+
+
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -22,10 +21,11 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } = this.state;
 
     return (
       <div className="details">
+      <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
