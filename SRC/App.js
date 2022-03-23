18348012807e23959a6efc001b7aafdb692f4184
Carlos Hernandez Topete <chtopete@gmail.com>
2022-03-23 11:36:11 +0900

    react router installed

--- SRC/App.js
+++ SRC/App.js
@@ -1,14 +1,24 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <div>
+        <BrowserRouter>
+          <h1>Adopt Me!</h1>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
-
 
 render(<App />, document.getElementById("root"));
---
+++ SRC/Details.jsx
@@ -0,0 +1,6 @@
+const Details = () => {
+  return <h2>Whatever you want</h2>
+
+}
+
+export default Details;
